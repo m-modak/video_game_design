be9a7fb45ca7010c647ef802bb7b9136cb9b628a
m-modak <sarahm046@stemcollegiate.ca>
2025-02-13 10:46:07 -0700

    feb13.cs

---
+++ feb13.cs
@@ -0,0 +1,6 @@
+//intro to using the Console.WriteLine(""); function. Don't add the quotation marks to the brackets to create a break in the text. We briefly went over text places inside singular quotation marks and how they represented strings. Console.Write(""); prints a new text underneath the previous function. 
+Console.WriteLine("Congratulations! This is your first code!");
+Console.WriteLine();
+Console.WriteLine("Lalala.");
+Console.WriteLine("Congratulations!");
+Console.Write("You wrote your first lines of code.");
