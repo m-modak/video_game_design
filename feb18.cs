b124a2d40b2027847192c21a065ba63431ed4741
m-modak <sarahm046@stemcollegiate.ca>
2025-02-18 12:56:25 -0700

    feb18.cs

---
+++ feb18.cs
@@ -0,0 +1,11 @@
+var message1 = "Bob";
+var message2 = "3";
+var message3 = "34.4";
+Console.Write("Hello, ");
+Console.Write(message1);
+Console.Write("! You have");
+Console.Write(message2);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(message3);
+Console.Write(" celsius.");
+//intro to variables and using different strings. 
