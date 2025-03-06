6eadd4b3874bec2b0db339da310951dd98969343
m-modak <sarahm046@stemcollegiate.ca>
2025-03-06 10:17:30 -0700

    mar6.cs

---
+++ mar6.cs
@@ -0,0 +1,20 @@
+print ("which ace attorney character are you?")
+print("answer these yes or no questions to find out!")
+print()
+phoenix = input ("""has you mentor died and left you her law office, only for you to find out her past, create a name for yourself, 
+ruin your reputation when you present forged evidence, then become a lawyer again and raise a child?""")
+if phoenix == "yes":
+  print ("so you're phoenix wright, the greatest defense lawyer!")
+
+print()
+miles = input("""have you watched you father die and believed you did it for 15 years, 
+only to be haunted by it again and realizing the man that raised you all that time was his murderer?""")
+if miles =="yes":
+  print("you're miles edgeworth, the demon prosecutor!")
+print()
+
+apollo = input("""have you been raised as an orphan your entire life and became a lawyer in hopes of pursuing justice, only to realize that your mother has been alive this entire time, your mentor is a cold-blooded killer, and you're in love with his brother?""")
+if apollo == "yes":
+  print("you're apollo justice and you're fine!")
+else:
+  print("i guess you're not like anyone from ace attorney...")
